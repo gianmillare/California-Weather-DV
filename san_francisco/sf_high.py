3a7192d02bc512849a8ae883e589e9c70049d0eb
gianmillare <gianmillaregsm@gmail.com>
2020-03-19 21:19:03 -0700

    completed displaying the indexes for each header

--- san_francisco/sf_high.py
+++ san_francisco/sf_high.py
@@ -1,8 +1,17 @@
 import csv
 
-filename = '../data/sanfrancisco_weather_04-2019_simple.csv'
+filename = '../data/sanfrancisco_weather_04-2019_simple.csv' # assign the file to a variable
 
+# # Part 1: Read the file
+# with open(filename) as f: # open the file
+#     reader = csv.reader(f) # create a reader object associated with the file
+#     header_row = next(reader) # assign a variable to the next row in the csv file
+#     # print(header_row)
+
+## Part 2: Print the headers and their positions
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+
+    for index, column_header in enumerate(header_row): # To find the index of each header, use the enumerate function
+        print(index, column_header)
