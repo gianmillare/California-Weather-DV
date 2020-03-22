595e83d7f089ff9684ee2c98dffbb18ef2db66fc
gianmillare <gianmillaregsm@gmail.com>
2020-03-21 18:24:18 -0700

    included datetime formatting for the x_axis

--- san_francisco/sf_high.py
+++ san_francisco/sf_high.py
@@ -1,4 +1,6 @@
 import csv
+
+from datetime import datetime
 import matplotlib.pyplot as plt
 
 filename = '../data/sanfrancisco_weather_04-2019_simple.csv' # assign the file to a variable
@@ -36,25 +38,55 @@ filename = '../data/sanfrancisco_weather_04-2019_simple.csv' # assign the file t
 
 
 
-# Part 4: Plot the high temperatures
+# # Part 4: Plot the high temperatures
+# with open(filename) as f:
+#     reader = csv.reader(f)
+#     header_row = next(reader)
+#
+#     highs = []
+#     for row in reader:
+#         high = int(row[5])
+#         highs.append(high)
+#
+# # Begin plotting
+# plt.style.use('seaborn')
+# fig, ax = plt.subplots()
+# ax.plot(highs, c='red')
+#
+# # Format the plot
+# plt.title('San Francisco High Temperature April 2019', fontsize=20)
+# plt.xlabel('', fontsize=14)
+# plt.ylabel('Temperatures (F)', fontsize=14)
+# plt.tick_params(axis='both', which='major', labelsize=14)
+# plt.savefig('../plots/sf1.png', dpi=100)
+# plt.show()
+
+
+
+# Part 5: Plotting the Date and Time
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    highs = []
+    # Get the date and high temperatures
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d') # Must match date format on CSV file
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
 
-# Begin plotting
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 
-# Format the plot
-plt.title('San Francisco High Temperature April 2019', fontsize=20)
+plt.title("SF - Daily High temperatures, April 2019", fontsize=20)
+
 plt.xlabel('', fontsize=14)
+fig.autofmt_xdate() #this is what you use to label x_axis with the date
+
 plt.ylabel('Temperatures (F)', fontsize=14)
 plt.tick_params(axis='both', which='major', labelsize=14)
-plt.savefig('../plots/sf1.png', dpi=100)
+
+plt.savefig('../plots/sf2.png', dpi=100)
 plt.show()
